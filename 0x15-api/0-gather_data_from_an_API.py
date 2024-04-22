0e72e882b7e193eb1e089cc35c1a545aff2afa99
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-04-22 13:03:27 +0100

    gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
     user_response = requests.get(url + "users/{}".format(employee_id))
     user = user_response.json()
-    params = {"userid": employee_id}
-    todo_response = requests.get(url + "todos", params=params)
+    params = {"userId": employee_id}
+    todo_response = requests.get(url + "todos", params)
     todos = todo_response.json()
     complete_response = []
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         if todo.get("completed") is True:
             complete_response.append(todo.get("title"))
 
-    print("Employee {} is done with tasks({}/{})".format(user.get("name"), len(complete_response), len(todos)))
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(complete_response), len(todos)))
 
     for response in complete_response:
         print("\t {}".format(response))

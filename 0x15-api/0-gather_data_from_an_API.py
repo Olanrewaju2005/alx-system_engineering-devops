70740cdb6ac6410618b60f154b4e3113b70450ce
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-04-22 12:50:26 +0100

    gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    employee_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
+    params = {"userid": employee_id}
+    todo_response = requests.get(url + "todos", params=params)
+    todos = todo_response.json()
+    complete_response = []
+
+    for todo in todos:
+        if todo.get("completed") is True:
+            complete_response.append(todo.get("title"))
+
+    print("Employee {} is done with tasks({}/{})".format(user.get("name"), len(complete_response), len(todos)))
+
+    for response in complete_response:
+        print("\t {}".format(response))
